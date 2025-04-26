d31448e6f846ba2ceaaf2cc8d4fea473628d264a
Rafael Villaça <rafaelvillaca1903@gmail.com>
2025-04-25 23:03:24 -0300

    tests: learning about mocks and starting implementation

--- tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -1,4 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Users.Register;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 
@@ -21,6 +23,10 @@ public class RegisterUserUseCaseTest
 
     private RegisterUserUseCase CreateUseCase()
     {
-        return new RegisterUserUseCase(null, null, null, null, null, null);
+        var mapper = MapperBuilder.Build();
+        var unitOfWork = UnitOfWorkBuilder.Build();
+        var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
+
+        return new RegisterUserUseCase(mapper, null, null, writeRepository, unitOfWork, null);
     }
 }
