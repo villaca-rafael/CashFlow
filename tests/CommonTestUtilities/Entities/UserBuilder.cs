5d6ca687a900c3bd5edcef398a1c26d43c786466
Rafael Villaça <rafaelvillaca1903@gmail.com>
2025-04-28 11:39:41 -0300

    tests: starting unit test for login usecase

---
+++ tests/CommonTestUtilities/Entities/UserBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Cryptography;
+
+namespace CommonTestUtilities.Entities;
+
+public class UserBuilder
+{
+    public static User Build()
+    {
+        var passwordEncripter = PasswordEncripterBuilder.Build();
+
+        var user = new Faker<User>()
+            .RuleFor(user => user.Id, _ => 1)
+            .RuleFor(user => user.Name, faker => faker.Person.FirstName)
+            .RuleFor(user => user.Email, faker => faker.Internet.Email())
+            .RuleFor(user => user.Password, (_, user) => passwordEncripter.Encrypt(user.Password))
+            .RuleFor(user => user.UserIdentifier, _ => Guid.NewGuid());
+
+        return user;
+    }
+}
--- tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
+++ tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Domain.Repositories.Users;
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories.Users;
 using Moq;
 
 namespace CommonTestUtilities.Repositories;
@@ -15,6 +16,12 @@ public class UserReadOnlyRepositoryBuilder
     public void ExistActiveUserWithEmail(string email)
     {
         _repository.Setup(userReadOnly => userReadOnly.ExistActiveUserWithEmail(email)).ReturnsAsync(true);;
+    }
+
+    public UserReadOnlyRepositoryBuilder GetUserByEmail(User user)
+    {
+        _repository.Setup(userReadOnly => userReadOnly.GetUserByEmail(user.Email)).ReturnsAsync(user);
+        return this;
     }
 
     public IUserReadOnlyRepository Build() => _repository.Object;
